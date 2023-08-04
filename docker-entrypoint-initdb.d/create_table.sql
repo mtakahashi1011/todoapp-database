89e85bec31551384d4fe7a98db57dd74aa92dc78
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-08-04 03:00:34 -0700

    Update create_table.sql

--- docker-entrypoint-initdb.d/create_table.sql
+++ docker-entrypoint-initdb.d/create_table.sql
@@ -1,8 +1,16 @@
 SET CHARACTER_SET_CLIENT = utf8;
 SET CHARACTER_SET_CONNECTION = utf8;
 
-CREATE TABLE users (
+CREATE DATABASE `todo_app`;
+
+CREATE TABLE `todo_app`.`users` (
   id          INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name        VARCHAR(255) UNIQUE,
   password    VARCHAR(255)
   );
+
+CREATE TABLE `todo_app`.`tasks` (
+  id          INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_id     INT NOT NULL,
+  content     VARCHAR(255) NOT NULL
+);
