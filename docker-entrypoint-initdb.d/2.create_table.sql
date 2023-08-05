3a5c47c478d5dc21c6ddfdca99537d684423ec36
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-08-05 02:32:31 -0700

    Update 2.create_table.sql

--- docker-entrypoint-initdb.d/2.create_table.sql
+++ docker-entrypoint-initdb.d/2.create_table.sql
@@ -1,10 +1,6 @@
 SET CHARACTER_SET_CLIENT = utf8;
 SET CHARACTER_SET_CONNECTION = utf8;
 
--- CREATE DATABASE 'keiba';
-
--- USE 'todo_app';
-
 CREATE TABLE IF NOT EXISTS 'todo_app'.'users' (
   `id`          INT UNSIGNED AUTO_INCREMENT NOT NULL,
   `name`        VARCHAR(255) UNIQUE,
@@ -18,5 +14,3 @@ CREATE TABLE IF NOT EXISTS 'todo_app'.'tasks' (
   `content`     VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
-
--- INSERT INTO 'todo_app'.'tasks' VALUES (1, 1, "Node.js");
