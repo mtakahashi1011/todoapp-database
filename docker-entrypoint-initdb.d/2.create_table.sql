aeac5881472a82092dfeef05c40ab7807035f819
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-08-05 02:31:14 -0700

    Update 2.create_table.sql

--- docker-entrypoint-initdb.d/2.create_table.sql
+++ docker-entrypoint-initdb.d/2.create_table.sql
@@ -6,17 +6,17 @@ SET CHARACTER_SET_CONNECTION = utf8;
 -- USE 'todo_app';
 
 CREATE TABLE IF NOT EXISTS 'todo_app'.'users' (
-  "id"          INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  "name"        VARCHAR(255) UNIQUE,
-  "password"    VARCHAR(255),
-  PRIMARY KEY ("id")
+  `id`          INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  `name`        VARCHAR(255) UNIQUE,
+  `password`    VARCHAR(255),
+  PRIMARY KEY (`id`)
   );
 
 CREATE TABLE IF NOT EXISTS 'todo_app'.'tasks' (
-  "id"          INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  "user_id"     INT NOT NULL,
-  "content"     VARCHAR(255) NOT NULL,
-  PRIMARY KEY ("id")
+  `id`          INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  `user_id`    INT NOT NULL,
+  `content`     VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 -- INSERT INTO 'todo_app'.'tasks' VALUES (1, 1, "Node.js");
