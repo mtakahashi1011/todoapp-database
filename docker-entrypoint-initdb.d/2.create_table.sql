9960959a89d2e1c70ead155887f33a34f590fe00
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-08-05 01:09:15 -0700

    Update 2.create_table.sql

--- docker-entrypoint-initdb.d/2.create_table.sql
+++ docker-entrypoint-initdb.d/2.create_table.sql
@@ -3,14 +3,18 @@ SET CHARACTER_SET_CONNECTION = utf8;
 
 CREATE DATABASE 'keiba';
 
+USE 'todo_app';
+
 CREATE TABLE 'todo_app'.'users' (
   'id'          INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   'name'        VARCHAR(255) UNIQUE,
   'password'    VARCHAR(255)
   );
 
-CREATE TABLE `todo_app`.`tasks` (
-  `id`          INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  `user_id`     INT NOT NULL,
-  `content`     VARCHAR(255) NOT NULL
+CREATE TABLE 'todo_app'.'tasks' (
+  'id'          INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  'user_id'     INT NOT NULL,
+  'content'     VARCHAR(255) NOT NULL
 );
+
+INSERT INTO 'todo_app'.'tasks' (user_id, content) values (1, "Node.js");
